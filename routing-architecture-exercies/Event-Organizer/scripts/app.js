1ac7ee310d07e83ff2f2a11f915a5b70a0071c24
vasIvanov <viivanov.info@gmail.com>
2019-04-25 12:34:07 +0300

    Separating html into templates

---
+++ routing-architecture-exercies/Event-Organizer/scripts/app.js
@@ -0,0 +1,20 @@
+const handlers = {}
+
+$(() => {
+  const app = Sammy('#root', function () {
+    this.use('Handlebars', 'hbs');
+    // home page routes
+    this.get('#/', handlers.getHome);
+
+    // user routes
+    this.get('#/register', handlers.getRegister);
+    this.get('#/login', handlers.getLogin);
+
+    this.post('#/register', handlers.registerUser);
+    this.post('#/login', handlers.loginUser);
+    this.get('#/logout', handlers.logoutUser);
+
+    // ADD YOUR ROUTES HERE
+  });
+  app.run('#/');
+});
